0b6764b5e2019d4db54434580681e681b31fc2ec
Ruggero <ruggero.bonanno@gmail.com>
2021-03-23 11:05:03 +0100

    mobile menu

--- java.js
+++ java.js
@@ -1,8 +1,11 @@
 function mobilecheck() {
     var x = document.getElementById("menu");
+    var menu = document.getElementById("mobile");
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         x.style.display = "none";
+        menu.style.display = "block"
       } else {
         x.style.display = "block";
+        menu.style.display = "none"
       }
   };
