57a50c0ed7b045cd3e93995a46b44df81a773625
Ruggero <ruggero.bonanno@gmail.com>
2021-03-28 14:54:52 +0200

    change font size dynamic

--- java.js
+++ java.js
@@ -1,15 +1,18 @@
 function mobilecheck() {
     var x = document.getElementById("menu");
     var menu = document.getElementById("mobile");
-    var main = document.getElementById("mobile");
+    var main = document.getElementsByClassName("centered_top");
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         x.style.display = "none";
-        menu.style.display = "block"
+        menu.style.display = "block";
         main.style.fontSize = "3vw"
 
       } else {
         x.style.display = "block";
-        menu.style.display = "none"
+        menu.style.display = "none";
+        for (var i = 0; i < x.length; i++) {
+            main[i].style.fontSize="20px";
+        }
       }
   };
 
